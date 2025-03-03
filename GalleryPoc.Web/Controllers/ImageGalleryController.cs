0474d0d295294b691a432ca7827027712b83e535
illiaalifanov <aliphanov.i@gmail.com>
2025-03-03 10:51:42 +0200

    poc start

---
+++ GalleryPoc.Web/Controllers/ImageGalleryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GalleryPoc.Web.Controllers
+{
+    
+    public class ImageGalleryController : Controller
+    {
+        private readonly string rootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot//uploads");
+        public IActionResult Index()
+        {
+            bool folderExists = Directory.Exists(rootPath);
+            if (!folderExists) 
+            {
+                Directory.CreateDirectory(rootPath);
+            }
+
+            List<string> imageNames = Directory.GetFiles(rootPath).Select(Path.GetFileName).ToList();
+            
+            return View(imageNames);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            if (file != null)
+            {
+                var path = Path.Combine(rootPath, Guid.NewGuid() + Path.GetExtension(file.FileName));
+                using (var fs = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(fs);
+                }
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ GalleryPoc.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GalleryPoc.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
